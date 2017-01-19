1aac62cbddc4fdbb298b24c85179263bcad2262d
NOLogicalSense <eddyrogersiv@gmail.com>
2017-01-19 10:00:15 -0700

    Create Runner.java

---
+++ Java Code/Animals/Runner.java
@@ -0,0 +1,32 @@
+
+public class Runner {
+	
+	public static void main(String[] args){
+		Pigeon bob = new Pigeon();
+		Pigeon steve = new Pigeon("What the fuck did you just fucking say about me, "
+				+ "you little bitch? I’ll have you know I graduated top of my class\n"
+				+ "in the Navy Seals, and I’ve been involved in numerous secret raids "
+				+ "on Al-Quaeda, and I have over 300 confirmed kills. I am trained in\n"
+				+ "gorilla warfare and I’m the top sniper in the entire US armed forces."
+				+ "You are nothing to me but just another target. I will wipe you the\n"
+				+ "fuck out with precision the likes of which has never been seen before"
+				+ "on this Earth, mark my fucking words. You think you can get away\n"
+				+ "with saying that shit to me over the Internet? Think again, fucker. "
+				+ "As we speak I am contacting my secret network of spies across the USA\n"
+				+ "and your IP is being traced right now so you better prepare for the "
+				+ "storm, maggot. The storm that wipes out the pathetic little thing\n"
+				+ "you call your life. You’re fucking dead, kid. I can be anywhere,"
+				+ "anytime, and I can kill you in over seven hundred ways, and that’s\n"
+				+ "just with my bare hands. Not only am I extensively trained in"
+				+ "unarmed combat, but I have access to the entire arsenal of the\n"
+				+ "United States Marine Corps and I will use it to its full extent"
+				+ "to wipe your miserable ass off the face of the continent, you\n"
+				+ "little shit. If only you could have known what unholy retribution"
+				+ "your little “clever” comment was about to bring down upon you,\n"
+				+ "maybe you would have held your fucking tongue. But you couldn’t,"
+				+ "you didn’t, and now you’re paying the price, you goddamn idiot.\n"
+				+ "I will shit fury all over you and you will drown in it. You’re"
+				+ "fucking dead, kiddo.",4,false);
+		System.out.println(steve.toString());
+	}
+}
